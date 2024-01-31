00dbc17866936bbcd1adfc1cc4e9b946b3b234c5
Sören Maschmann <smaschmann@slashwhy.de>
2024-01-31 12:23:17 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::{
+    env::{current_exe, set_current_dir},
+    process::Command,
+};
+
+fn main() {
+    let directory = current_exe().expect("failed to get current folder");
+    set_current_dir(directory.parent().unwrap()).expect("failed to set current folder");
+
+    let codesign_output = Command::new("codesign")
+        .arg("--force")
+        .arg("--deep")
+        .arg("--sign")
+        .arg("-")
+        .arg(r"shapez 2.app")
+        .output()
+        .expect("failed to execute codesign");
+    println!("codesign: {:?}", codesign_output);
+
+    let quarantine_output = Command::new("xattr")
+        .arg("-d")
+        .arg("-r")
+        .arg("com.apple.quarantine")
+        .arg(r"shapez 2.app")
+        .output()
+        .expect("failed to execute quarantine");
+    println!("quarantine: {:?}", quarantine_output);
+
+    let apply_output = Command::new("chmod")
+        .arg("+x")
+        .arg(r"shapez 2.app/Contents/MacOS/shapez 2")
+        .output()
+        .expect("failed to apply exectuable");
+    println!("apply: {:?}", apply_output);
+}
